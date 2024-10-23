6171bc0e951cbe896c9e0db2db5d4d618772a874
andreapdr <andrea.pedrotti@isti.cnr.ir>
2024-10-23 17:10:37 +0200

    load llama3.1 70B on correct gpus

--- evaluate_exemplars2cat.py
+++ evaluate_exemplars2cat.py
@@ -89,7 +89,7 @@ def get_model(model_name="llava"):
         processor = None
     elif model_name.lower() == "llama3.1-70b":
         tokenizer = AutoTokenizer.from_pretrained(LLAMA_31_70B, token=os.getenv("MY_HF_TOKEN"))
-        model = AutoModelForCausalLM.from_pretrained(LLAMA_31_70B, torch_dtype=torch.float16, low_cpu_mem_usage=True, device_map="auto", token=os.getenv("MY_HF_TOKEN")).to(DEVICE)
+        model = AutoModelForCausalLM.from_pretrained(LLAMA_31_70B, torch_dtype=torch.float16, low_cpu_mem_usage=True, device_map="auto", token=os.getenv("MY_HF_TOKEN"))
         processor = None
     
     model.eval()
