7f550a836e45551f575064905d32cfbb047eaf0b
andreapdr <andrea.pedrotti@isti.cnr.it>
2024-11-08 09:55:33 +0100

    query sketchengine update

--- query_sketchengine.py
+++ query_sketchengine.py
@@ -1,19 +1,19 @@
+import os
 import csv
 import pandas as pd
 import requests
 import time
+from dotenv import load_dotenv
+load_dotenv(".secrets.env")
 
 from tqdm import tqdm
 
+"""
+The FUP limit applies to the number of requests (not to the volume of data transferred) per one minute, one hour and one day and defaulting to 100, 900 and 2,000 requests, respectivel
+"""
 
-USERNAME = 'andrea.pedrotti'
-API_KEY = '10946b2a5d6e1307320873b0dacdd973'
 
-# USERNAME = 'giulia.rambelli4'
-# API_KEY = 'a07d33b5d17643f736bff204acbf5750'
-
-# USERNAME = 'caterina.villani6'
-# API_KEY = 'f60a89d26e4982674bf65948c5d1f442'
+API_DAY_LIMIT = 2000
 
 base_url = 'https://api.sketchengine.eu/bonito/run.cgi'
 corpus = 'preloaded/ittenten20_fl1'
@@ -40,74 +40,76 @@ def get_freqs(expression, cname, username, api_key):
         response.raise_for_status()
 
 
-def query_sketchengine(data, corpus, username, api_key, sleeptime, outfile):
-    API_LIMIT = 500
-    api_calls = 0
-
-    try:
-        existing_data = pd.read_csv(outfile)
-        already_queried = set(existing_data['exemplar'])
-    except FileNotFoundError:
-        already_queried = set()
-
-    # Open the output file in append mode
-    with open(outfile, 'a', newline='', buffering=1) as csvfile:
-        fieldnames = ['exemplar', 'abs_freq', 'rel_freq']
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        
-        # Write the header if the file was empty or new
-        if csvfile.tell() == 0:
-            writer.writeheader()
-
-        for i, exemplar in enumerate(tqdm(data)):
-            if exemplar not in already_queried:
-                if pd.isna(exemplar) or exemplar == "":
-                    abs_freq = 0
-                    rel_freq = 0.0
-                else:
-                    abs_freq, rel_freq = get_freqs(expression=exemplar, cname=corpus, username=username, api_key=api_key)
-                    api_calls += 1
-                
-                # Append (exemplar, abs_freq, rel_freq) to the output file
-                # print(f"writing: {exemplar}")
-                writer.writerow({'exemplar': exemplar, 'abs_freq': abs_freq, 'rel_freq': rel_freq})
-                csvfile.flush()  # Ensure the data is written to the file
-                already_queried.add(exemplar)
-
-                if i % 100 == 0 and i != 0:
-                    time.sleep(sleeptime)
-
-    return
+def get_user_data(user):
+    USERNAME = os.getenv(f"USERNAME_{user.upper()}")
+    API_KEY = os.getenv(f"USERNAME_{user.upper()}")
+    return USERNAME, API_KEY
 
 
-def fill_df(df):
-    new_freqs = []
+def main(args):
+    _temp = "temp_" + str(args.temp).replace(".", "")
 
-    for i, row in tqdm(df.iterrows(), total=df.shape[0]):
-        if pd.isna(row.abs_freq):
-            if pd.isna(row.exemplar):
-                new_freqs.append(0.0)
-            else:
-                time.sleep(1)
-                abs_freq, _ = get_freqs(expression=row.exemplar, cname="preloaded/ittenten20_fl1", username=USERNAME, api_key=API_KEY)
-                new_freqs.append(abs_freq)
+    assert args.modality in ["visual", "textual"]
+
+    USERNAME, API_KEY = get_user_data(args.user)
+
+    exemplars_f = f"results-generation/italian/formatted/{_temp}/{args.model}-it-{args.modality}-{_temp}_alliters.csv"
+    all_freqs_f = "results-generation/freqs/all/all-freqs-sketchengine.csv"
+    out_f = f"results-generation/freqs/{args.model}-{args.modality}/{_temp}"
+    out_all_f = f"results-generation/freqs/all/all-freqs-sketchengine-{args.user}.csv"
+
+    os.makedirs(out_f, exist_ok=True)
+
+    exemplars = pd.read_csv(exemplars_f)
+    all_freqs = pd.read_csv(all_freqs_f)
+    all_freqs_exemplars = all_freqs.exemplar.tolist()
+
+    if args.resume:
+        print(f"- resuming from {out_f}/freqs.csv !")
+        model_freqs = pd.read_csv(f"{out_f}/freqs.csv") 
+    else:
+        model_freqs = pd.DataFrame(columns=["exemplar", "abs_freq", "rel_freq"])
+
+    # remove multiple occurrences of the same generated exemplar
+    exemplars = exemplars.drop_duplicates(subset="exemplar")
+    # print(exemplars.head())
+
+    api_hits = 0
+    for i, row in tqdm(exemplars.iterrows(), total=exemplars.shape[0]):
+        exemplar = row.exemplar
+        if exemplar in all_freqs_exemplars:
+            match_row = all_freqs[all_freqs.exemplar == exemplar].iloc[0]
+            abs_freq = match_row.abs_freq
+            rel_freq = match_row.rel_freq
+            model_freqs.loc[len(model_freqs)] = [exemplar, abs_freq, rel_freq]
         else:
-            new_freqs.append(row.abs_freq)
-    
-    df["abs_freq"] = new_freqs
+            time.sleep(0.25)
+            abs_freq, rel_freq = get_freqs(expression=exemplar, cname=corpus, username=USERNAME, api_key=API_KEY)
+            model_freqs.loc[len(model_freqs)] = [exemplar, abs_freq, rel_freq]
+            api_hits += 1
+            model_freqs.to_csv(os.path.join(out_f, "freqs.csv"), index=False)
+            all_freqs = pd.concat([all_freqs, model_freqs]).drop_duplicates(subset=["exemplar"])
+            all_freqs.to_csv(out_all_f, index=False)
+        if api_hits >= API_DAY_LIMIT:
+            print(f"+++ Reached API DAY LIMIT ({API_DAY_LIMIT}) with user: {USERNAME}")
+            time.sleep(86400)   # way 24h and reset hits
+            api_hits = 0
 
-    return df
+    all_freqs = pd.concat([all_freqs, model_freqs]).drop_duplicates(subset=["exemplar"])
+    
+    print(f"saving {args.model} results in {out_f}/freqs.csv")
+    print(f"saving aggregated results in {out_all_f}")
+    model_freqs.to_csv(os.path.join(out_f, "freqs.csv"), index=False)
+    all_freqs.to_csv(out_all_f, index=False)
 
 
 if __name__ == "__main__":
-    FILEPATH = "data/llava/temperature00/visual-alliters.csv"
-    OUTFILE =  "data/freqs-sketchengine.csv" 
-
-    print(f"{FILEPATH=} - {OUTFILE=}")
-
-    df = pd.read_csv(FILEPATH)
-    
-    missing_freqs = df.abs_freq.isna().sum()
-    print(f"{FILEPATH} exemplars with nan freqs: {missing_freqs}")
-
-    query_sketchengine(df.exemplar, corpus, USERNAME, API_KEY, sleeptime=60, outfile=OUTFILE)
+    from argparse import ArgumentParser
+    parser = ArgumentParser()
+    parser.add_argument("--model", type=str, choices=["idefics2", "llama3.1", "llama3.1-70b", "nemo", "llava", "llama3.2", "mistral", "mixtral"])
+    parser.add_argument("--temp", type=float, default=0.5)
+    parser.add_argument("--modality", type=str, default="textual")
+    parser.add_argument("--user", type=str, default="andrea")
+    parser.add_argument("--resume", action="store_true")
+    args = parser.parse_args()
+    main(args)
